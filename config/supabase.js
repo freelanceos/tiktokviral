d6b52bf96d422157a78d4bbf9e5734b5f37bd226
freelanceos <freelanceos2025@gmail.com>
2025-05-28 00:15:05 +0300

    Add files via upload

--- config/supabase.js
+++ config/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+import { supabase } from '../config/supabase.js'
+
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
